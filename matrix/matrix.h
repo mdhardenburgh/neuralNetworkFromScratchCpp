d0bfc64e6b2736068a665983f1f75234ca2c226b
Matthew Hardenburgh <mdhardenburgh@protonmail.com>
2024-01-28 01:10:10 -0800

    got unit test to work on square matricies

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,5 +1,5 @@
-#ifndef MATRIX
-#define MATRIX
+#ifndef MATRIX_H
+#define MATRIX_H
 
 #include <stdint.h>
 #include <vector>
@@ -17,7 +17,7 @@ template <class T> class matrix
         matrix(T* data, const uint32_t& rows, const uint32_t& columns);
         ~matrix();
 
-        T at(const uint32_t& row, const uint32_t& column);
+        T at(const uint32_t& index);
         void transpose();
         //matrix add(matrix B);
         //void scalarMultiply(matrix B);
@@ -43,6 +43,8 @@ template <class T> matrix<T>::matrix(T* data, const uint32_t& rows, const uint32
     m_rows = rows;
     m_columns = columns;
 
+    m_data = new T[m_rows*m_columns];
+
     if(m_rows < 1)
     {
         std::cout<<__PRETTY_FUNCTION__<<": m_rows is less than 1!!!!"<<std::endl;
@@ -59,82 +61,41 @@ template <class T> matrix<T>::matrix(T* data, const uint32_t& rows, const uint32
         assert(false);
     }
 
-    m_data.resize(m_rows);
-    for(uint32_t iter = 0; iter < m_data.size(); iter++)
+    for(uint32_t iIter = 0; iIter < m_rows * m_columns; iIter++)
     {
-        m_data.at(iter).resize(m_columns);
+        m_data[iIter] = data[iIter];
     }
 
-    //if MxN matrix (rows x columns)
-    if((m_rows > 1)&&(m_columns > 1))
-    {
-        for(uint32_t iIter = 0; iIter < m_rows; iIter++)
-        {
-            for(uint32_t jIter = 0; jIter < m_columns; jIter++)
-            {
-                (m_data)[iIter][jIter] = data[iIter][jIter];
-            }
-        }
-    }
-
-    // Mx1 matrix 
-    if((m_rows > 1)&&(m_columns == 1))
-    {
-        for(uint32_t iIter = 0; iIter < m_rows; iIter++)
-        {
-            for(uint32_t jIter = 0; jIter < m_columns; jIter++)
-            {
-                (m_data)[iIter][jIter] = reinterpret_cast<T*>(data)[iIter];
-            }
-        }
-    }
-
-    // 1XN matrix
-    if((m_rows == 1)&&(m_columns > 1))
-    {
-        for(uint32_t iIter = 0; iIter < m_rows; iIter++)
-        {
-            for(uint32_t jIter = 0; jIter < m_columns; jIter++)
-            {
-                (m_data)[iIter][jIter] = reinterpret_cast<T*>(data)[jIter];
-            }
-        }
-    }
 }
 
 template <class T> matrix<T>::~matrix()
 {
-    //delete m_data;
+    delete m_data;
 }
 
-template <class T> T matrix<T>::at(const uint32_t& row, const uint32_t& column)
+template <class T> T matrix<T>::at(const uint32_t& index)
 {
-    return (m_data)[row][column];
+    return m_data[index];
 }
 
 template <class T> void matrix<T>::transpose()
 {
-    std::vector<std::vector<T>> temp = m_data;
+    T* temp = m_data;
+    m_data = new T[m_rows * m_columns];
     uint32_t newRows = m_columns;
     uint32_t newColumns = m_rows;
-
-    m_data.resize(newRows);
-    for(uint32_t iIter = 0; iIter < m_data.size(); iIter++)
-    {
-        m_data.at(iIter).resize(newColumns);
-    }
 
     for(uint32_t iIter = 0; iIter < m_rows; iIter++)
     {
         for(uint32_t jIter = 0; jIter < m_columns; jIter++)
         {
-            (m_data)[iIter][jIter] = temp[jIter][iIter];
-        }
+            m_data[iIter + (jIter * m_columns)] = temp[jIter + (iIter * m_rows)];
+        }       
     }
 
     m_rows = newRows;
     m_columns = newColumns;
-
+    delete temp;
 }
 
-#endif //MATRIX
+#endif //MATRIX_H
--- matrix/unitTest/matrixTest.cpp
+++ matrix/unitTest/matrixTest.cpp
@@ -1,32 +1,116 @@
+#include <iostream>
 #include <gtest/gtest.h>
+
 #include "matrix.h"
 
-TEST(matrixTest, test_transpose_function)
+TEST(matrixTest, test_transpose_function_square_matrix)
 {
-    uint32_t testMatrix[5][5] 
+    const uint32_t rows = 5;
+    const uint32_t columns = 5;
+    
+    uint32_t testMatrix[rows * columns] 
     {
-        {1, 2, 3, 4, 5}, 
-        {1, 2, 3, 4, 5}, 
-        {1, 2, 3, 4, 5}, 
-        {1, 2, 3, 4, 5}, 
-        {1, 2, 3, 4, 5}
+        1, 2, 3, 4, 5, 
+        1, 2, 3, 4, 5, 
+        1, 2, 3, 4, 5, 
+        1, 2, 3, 4, 5, 
+        1, 2, 3, 4, 5
     };
-    uint32_t expected[5][5] 
+    
+    uint32_t expected[rows * columns] 
     {
-        {1, 1, 1, 1, 1},
-        {2, 2, 2, 2, 2},
-        {3, 3, 3, 3, 3},
-        {4, 4, 4, 4, 4},
-        {5, 5, 5, 5, 5}                    
+        1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3,
+        4, 4, 4, 4, 4,
+        5, 5, 5, 5, 5                    
     };
-    matrix<uint32_t> myMatrix(reinterpret_cast<uint32_t*>(testMatrix), 5, 5);
-    myMatrix.transpose();
-    for(uint32_t iIter = 0; iIter < 5; iIter++)
+    
+    matrix<uint32_t> myMatrix(testMatrix, rows, columns);
+
+    //print out matrix before transpose
+    for(uint32_t iIter = 0; iIter < (rows * columns); iIter++)
     {
-        for(uint32_t jIter = 0; jIter < 5; jIter++)
+        if(iIter%columns == 0)
         {
-            EXPECT_EQ(expected[iIter][jIter], myMatrix.at(iIter, jIter));
+            std::cout<<std::endl;
         }
+        
+        std::cout<<myMatrix.at(iIter)<<" ";
+    }
+    std::cout<<std::endl;
+
+    myMatrix.transpose();
+
+    //print out matrix after transpose
+    for(uint32_t iIter = 0; iIter < (rows * columns); iIter++)
+    {
+        if(iIter%columns == 0)
+        {
+            std::cout<<std::endl;
+        }
+        
+        std::cout<<myMatrix.at(iIter)<<" ";
+    }
+    std::cout<<std::endl;
+
+    for(uint32_t iIter = 0; iIter < (rows * columns); iIter++)
+    {
+        EXPECT_EQ(expected[iIter], myMatrix.at(iIter));
+    }
+}
+
+TEST(matrixTest, test_transpose_function_not_square_matrix)
+{
+    const uint32_t rows = 2;
+    const uint32_t columns = 5;
+    
+    uint32_t testMatrix[rows * columns] 
+    {
+        1, 2, 3, 4, 5, 
+        1, 2, 3, 4, 5
+    };
+    
+    uint32_t expected[rows * columns] 
+    {
+        1, 1,
+        2, 2,
+        3, 3,
+        4, 4,
+        5, 5                    
+    };
+    
+    matrix<uint32_t> myMatrix(testMatrix, rows, columns);
+
+    //print out matrix before transpose
+    for(uint32_t iIter = 0; iIter < (rows * columns); iIter++)
+    {
+        if(iIter%columns == 0)
+        {
+            std::cout<<std::endl;
+        }
+        
+        std::cout<<myMatrix.at(iIter)<<" ";
+    }
+    std::cout<<std::endl;
+
+    myMatrix.transpose();
+
+    //print out matrix after transpose
+    for(uint32_t iIter = 0; iIter < (rows * columns); iIter++)
+    {
+        if(iIter%columns == 0)
+        {
+            std::cout<<std::endl;
+        }
+        
+        std::cout<<myMatrix.at(iIter)<<" ";
+    }
+    std::cout<<std::endl;
+
+    for(uint32_t iIter = 0; iIter < (rows * columns); iIter++)
+    {
+        EXPECT_EQ(expected[iIter], myMatrix.at(iIter));
     }
 }
 
